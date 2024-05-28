ccc8174fed6a7712cbbdb258486a31ed9a0877f5
marcoa1818 <marcochetos10@gmail.com>
2024-05-28 10:03:23 -0600

    Creacion de vista Registrar Usuario

---
+++ FrontEnd/front-gimnasio/src/components/RegisterUser.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>Registrar Usuario</h1>
+</template>
--- FrontEnd/front-gimnasio/src/router/index.js
+++ FrontEnd/front-gimnasio/src/router/index.js
@@ -1,14 +1,22 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '@/components/Login.vue'
+import RegisterUser from '@/components/RegisterUser.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
+      name: 'login',
       component: LoginView
-    }
+    },
+
+    {
+      path: '/register',
+      name: 'register',
+      component: LoginView
+    },
+
   ]
 })
 
