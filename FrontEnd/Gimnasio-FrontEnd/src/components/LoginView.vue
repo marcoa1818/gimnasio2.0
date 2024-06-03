aeb30589bafff1e8cccc31fb90affbc58ba64ab8
marcoa1818 <marcochetos10@gmail.com>
2024-06-03 17:30:26 -0600

    Mover archivos de carpeta frontend, contiene vistas de registrar, login y menu

--- FrontEnd/Gimnasio-FrontEnd/src/components/LoginView.vue
+++ FrontEnd/Gimnasio-FrontEnd/src/components/LoginView.vue
@@ -1,20 +1,15 @@
 <template>
-    <!-- source:https://codepen.io/owaiswiz/pen/jOPvEPB -->
     <div class="min-h-screen bg-gray-100 text-gray-900 flex justify-center">
         <div class="max-w-screen-xl m-0 sm:m-10 bg-white shadow sm:rounded-lg flex justify-center flex-1">
             <div class="lg:w-1/2 xl:w-5/12 p-6 sm:p-12">
-                <div>
-                    <img src="https://storage.googleapis.com/devitary-image-host.appspot.com/15846435184459982716-LogoMakr_7POjrN.png"
-                        class="w-32 mx-auto" />
-                </div>
                 <div class="mt-12 flex flex-col items-center">
                     <h1 class="text-2xl xl:text-3xl font-extrabold">
-                        Registrar
+                        Sign up
                     </h1>
                     <div class="w-full flex-1 mt-8">
                         <div class="flex flex-col items-center">
                             <button
-                                class="w-full max-w-xs font-bold shadow-sm rounded-lg py-3 bg-indigo-100 text-gray-800 flex items-center justify-center transition-all duration-300 ease-in-out focus:outline-none hover:shadow focus:shadow-sm focus:shadow-outline">
+                                class="w-full font-bold shadow-sm rounded-lg py-3 bg-indigo-100 text-gray-800 flex items-center justify-center transition-all duration-300 ease-in-out focus:outline-none hover:shadow focus:shadow-sm focus:shadow-outline">
                                 <div class="bg-white p-2 rounded-full">
                                     <svg class="w-4" viewBox="0 0 533.5 544.3">
                                         <path
@@ -32,12 +27,12 @@
                                     </svg>
                                 </div>
                                 <span class="ml-4">
-                                    Registrar con Google
+                                    Sign Up with Google
                                 </span>
                             </button>
-
+    
                             <button
-                                class="w-full max-w-xs font-bold shadow-sm rounded-lg py-3 bg-indigo-100 text-gray-800 flex items-center justify-center transition-all duration-300 ease-in-out focus:outline-none hover:shadow focus:shadow-sm focus:shadow-outline mt-5">
+                                class="w-full  font-bold shadow-sm rounded-lg py-3 bg-indigo-100 text-gray-800 flex items-center justify-center transition-all duration-300 ease-in-out focus:outline-none hover:shadow focus:shadow-sm focus:shadow-outline mt-5">
                                 <div class="bg-white p-1 rounded-full">
                                     <svg class="w-6" viewBox="0 0 32 32">
                                         <path fill-rule="evenodd"
@@ -45,56 +40,59 @@
                                     </svg>
                                 </div>
                                 <span class="ml-4">
-                                    Registrar con GitHub
+                                    Sign Up with GitHub
                                 </span>
                             </button>
                         </div>
-
+    
                         <div class="my-12 border-b text-center">
                             <div
                                 class="leading-none px-2 inline-block text-sm text-gray-600 tracking-wide font-medium bg-white transform translate-y-1/2">
-                                O Registrar con Correo Electronico
+                                Or sign up with e-mail
                             </div>
                         </div>
-
-                        <div class="mx-auto max-w-xs">
+    
+                        <div class="w-full">
                             <input
                                 class="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
-                                type="email" placeholder="Correo Electronico" />
+                                type="email" placeholder="Email" />
                             <input
                                 class="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
-                                type="password" placeholder="Contraseña" />
-                            <button
-                                class="mt-5 tracking-wide font-semibold bg-indigo-500 text-gray-100 w-full py-4 rounded-lg hover:bg-indigo-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none">
-                                <svg class="w-6 h-6 -ml-2" fill="none" stroke="currentColor" stroke-width="2"
-                                    stroke-linecap="round" stroke-linejoin="round">
-                                    <path d="M16 21v-2a4 4 0 00-4-4H5a4 4 0 00-4 4v2" />
-                                    <circle cx="8.5" cy="7" r="4" />
-                                    <path d="M20 8v6M23 11h-6" />
-                                </svg>
-                                <span class="ml-3">
-                                    Registrar
-                                </span>
-                            </button>
+                                type="password" placeholder="Password" />
+                            <a href="/home">
+                                <button
+                                    class="mt-5 tracking-wide font-semibold bg-red-700 text-gray-100 w-full py-4 rounded-lg hover:bg-red-900 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none">
+                                    <svg class="w-6 h-6 -ml-2" fill="none" stroke="currentColor" stroke-width="2"
+                                        stroke-linecap="round" stroke-linejoin="round">
+                                        <path d="M16 21v-2a4 4 0 00-4-4H5a4 4 0 00-4 4v2" />
+                                        <circle cx="8.5" cy="7" r="4" />
+                                        <path d="M20 8v6M23 11h-6" />
+                                    </svg>
+                                    <span class="ml-3">
+                                        Sign Up
+                                    </span>
+                                </button>
+                            </a>
                             <p class="mt-6 text-xs text-gray-600 text-center">
-                                Estoy de acuerdo con Bulls Gym y sus
+                                I agree to abide by templatana's
                                 <a href="#" class="border-b border-gray-500 border-dotted">
-                                    Terminos de Servicio
+                                    Terms of Service
                                 </a>
-                                y su
+                                and its
                                 <a href="#" class="border-b border-gray-500 border-dotted">
-                                    Politica de Privacidad
+                                    Privacy Policy
                                 </a>
                             </p>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="flex-1 bg-indigo-100 text-center hidden lg:flex">
-                <div class="m-12 xl:m-16 w-full bg-contain bg-center bg-no-repeat"
-                    style="background-image: url('https://storage.googleapis.com/devitary-image-host.appspot.com/15848031292911696601-undraw_designer_life_w96d.svg');">
+            <div class="flex-1 bg-gray-500 text-center hidden lg:flex ">
+                <div class="m-12 xl:m-16 w-10/12 bg-contain bg-center bg-no-repeat "
+                    style="background-image: url('https://i.ibb.co/pdP0hjc/logo-gimnasio.png');"
+                    >
                 </div>
             </div>
         </div>
     </div>
-</template>
+    </template>
--- FrontEnd/Gimnasio-FrontEnd/src/router/index.js
+++ FrontEnd/Gimnasio-FrontEnd/src/router/index.js
@@ -1,6 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import LoginView from '@/components/Login.vue'
+import LoginView from '@/components/LoginView.vue'
 import RegisterUser from '@/components/RegisterUser.vue'
+import  Home from '@/components/Home.vue'
+import Menu from '@/components/Menu.vue'
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,13 +13,16 @@ const router = createRouter({
       name: 'login',
       component: LoginView
     },
-
     {
-      path: '/register',
-      name: 'register',
+      path: '/signup',
+      name: 'signup',
       component: RegisterUser
     },
-
+    {
+      path: '/home',
+      name: 'home',
+      component: Menu
+    }
   ]
 })
 
