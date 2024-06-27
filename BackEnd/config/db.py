20eb376e7ef2a4dcfcc95a2ed2a927137390f958
marcoa1818 <marcochetos10@gmail.com>
2024-06-27 11:46:44 -0600

    editar archivo de models, schemas, routes y crud para la API

--- BackEnd/config/db.py
+++ BackEnd/config/db.py
@@ -1,9 +1,21 @@
-from sqlalchemy import create_engine, MetaData
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-engine = create_engine("mysql+pymysql://root:1234@localhost:3307/test.db")
-meta = MetaData()
+DATABASE_URL = "mysql+pymysql://root:1234@localhost:3307/test"
 
-conn = engine.connect()
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocomit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
 
 
 
