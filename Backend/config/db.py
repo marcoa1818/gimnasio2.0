91fdf611504b06bb8797e2eee39bfda066e16c90
marcoa1818 <marcochetos10@gmail.com>
2024-07-22 15:39:17 -0600

    url de render

--- Backend/config/db.py
+++ Backend/config/db.py
@@ -2,9 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://avnadmin:AVNS_RSH4dsc8x0wTx6SWfV4@mysql-10da6282-amelipg.e.aivencloud.com:22252/defaultdb'
+SQLALCHEMY_DATABASE_URL = 'mysql://avnadmin:AVNS_J6l6rJV3ltPJNQPjH2G@mysql-837b9e5-gim-2-0.l.aivencloud.com:22456/defaultdb?ssl-mode=REQUIRED'
 # SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root:1234@localhost:3307/test'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
+
