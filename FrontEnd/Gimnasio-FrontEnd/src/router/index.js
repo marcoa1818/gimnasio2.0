ae59785c6fca4605e1ca75fad3500fe8a30da176
marcoa1818 <marcochetos10@gmail.com>
2024-08-08 11:11:58 -0600

    nueva carpeta de FrontEnd

--- FrontEnd/Gimnasio-FrontEnd/src/router/index.js
+++ FrontEnd/Gimnasio-FrontEnd/src/router/index.js
@@ -3,7 +3,13 @@ import LoginView from '@/components/LoginView.vue'
 import RegisterUser from '@/components/RegisterUser.vue'
 import  Home from '@/components/Home.vue'
 import Menu from '@/components/Menu.vue'
-
+import Persona from '@/components/Persona.vue'
+import ListaPersonas from '@/components/ListaPersonas.vue'
+import Ejercicio from '@/components/Ejercicio.vue'
+import Rutina from '@/components/Rutina.vue'
+import ProgramaSaludable from '@/components/ProgramaSaludable.vue'
+import RutinaEjercicio from '@/components/RutinaEjercicio.vue'
+import DetallePrograma from '@/components/DetallePrograma.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,14 +20,22 @@ const router = createRouter({
       component: LoginView
     },
     {
-      path: '/signup',
-      name: 'signup',
+      path: '/register',
+      name: 'register',
       component: RegisterUser
     },
     {
       path: '/home',
       name: 'home',
-      component: Menu
+      component: Menu,
+      children: [
+        {path: '/personas', name: 'personas', component: ListaPersonas},
+        {path: '/ejercicios', name: 'ejercicios', component: Ejercicio},
+        {path: '/rutinas', name: 'rutinas', component: Rutina},
+        {path: '/programas_saludables', name: 'programas_saludables', component: ProgramaSaludable},
+        {path: '/rutinas_ejercicios', name: 'rutinas_ejercicios', component: RutinaEjercicio},
+        {path: '/detalles_programas', name: 'detalles_programas', component: DetallePrograma},
+      ]
     }
   ]
 })
