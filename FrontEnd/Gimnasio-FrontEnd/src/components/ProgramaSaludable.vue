7e5f3035f76d7fdb4932dd5da3220b6752635680
marcoa1818 <marcochetos10@gmail.com>
2024-08-08 11:25:33 -0600

    Formulario de Porgrama saludable

--- FrontEnd/Gimnasio-FrontEnd/src/components/ProgramaSaludable.vue
+++ FrontEnd/Gimnasio-FrontEnd/src/components/ProgramaSaludable.vue
@@ -1,18 +1,40 @@
 <template>
     <form>
-        <h1 class="text-2xl xl:text-3xl font-extrabold">Porgramas Saludables</h1>
+        <h1 class="text-2xl xl:text-3xl font-extrabold">Programas Saludables</h1>
 
         <input
+            class="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
+            type="text" placeholder="ID del Programa"
+        /> 
+        <input
             class="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5 col-span-2"
-            type="text" placeholder="Nombre Completo"
+            type="text" placeholder="Nombre del Programa Saludable"
         />
         <input
             class="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
-            type="text" placeholder="Primer Apellido"
+            type="text" placeholder="Estatus"
         />
         <input
             class="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
-            type="text" placeholder="Segundo Apellido"
-        />        
+            type="text" placeholder="Duración"
+        />
+        <input
+            class="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
+            type="text" placeholder="Porcentaje de Avance"
+        />
+        <a href="/" class="col-span-2" >
+            <button
+                class="mt-5 tracking-wide font-semibold bg-red-700 text-gray-100 w-full py-4 rounded-lg hover:bg-red-900 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none col-span-2">
+                <svg class="w-6 h-6 -ml-2" fill="none" stroke="currentColor" stroke-width="2"
+                    stroke-linecap="round" stroke-linejoin="round">
+                    <path d="M16 21v-2a4 4 0 00-4-4H5a4 4 0 00-4 4v2" />
+                    <circle cx="8.5" cy="7" r="4" />
+                    <path d="M20 8v6M23 11h-6" />
+                </svg>
+                <span class="ml-3 uppercase">
+                    Registrar
+                </span>
+            </button>
+        </a>          
     </form>
 </template>
